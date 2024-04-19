769aa297fd7a5c7873bc4abb5dd2add1d85e2e91
ibanezfr <francoibanez.dev@gmail.com>
2024-04-19 10:42:29 -0300

    start solving market problem

---
+++ franco/19-04-24/TadProducto.py
@@ -0,0 +1,10 @@
+# Archivo: TadLibro.py
+# Autor:   Franco E. Ibañez
+
+def crearProducto():
+    #Crea un producto nuevo
+    # prod=['nombre', 'tipo', codigo]
+    prod=['', '', 0]
+    return prod
+
+
---
+++ franco/19-04-24/app.py
@@ -0,0 +1,18 @@
+# Archivo:  app.py
+# Autor:    Franco E. Ibañez
+
+# 3)a) Especificar el TAD Producto y TAD Supermeracado, para el manejo de
+# productos.
+#   b) Realizar una aplicacón que permita cargar todos los productos del
+# supermercado.
+#   c) Aumentarle un 5% el precio de los productos lácteos. Luego imprimir el 
+#   listado de productos completo.
+
+import TadProducto, TadSupermercado
+
+from TadSupermercado import *
+from TadProducto import *
+
+
+
+
